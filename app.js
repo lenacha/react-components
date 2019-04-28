83873a85923adaf2e4503b85357c241cd4efa1d9
Yerin Cha <lenacha32@gmail.com>
2019-04-28 16:53:21 -0700

    Implement app.js

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-// TODO
+var App = () => (
+  <div>
+    <Cucumbers />
+    <Kale />
+  </div>
+);
+
+var Cucumbers = () => (
+  <li>cucumber</li>
+);
+
+var Kale = () => (
+  <li>kale</li>
+);
+
+var groceryItems = ['soju', 'beer', 'pork belly']
+
+var GroceryList = () => (
+  <ul>
+    {groceryItems.map(item => <GroceryListItem item={item} />)}
+  </ul>
+);
+
+class GroceryListItem extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      toggle: false
+    };
+  }
+
+  makeBold() {
+    this.setState ({
+      toggle: !this.state.toggle 
+    });
+  }
+
+  render(){
+    var style={
+      fontWeight: this.state.toggle ? 'bold' : 'normal'
+    }
+    return(
+      <li style={style} onMouseEnter={this.makeBold.bind(this)} onMouseLeave={this.makeBold.bind(this)}>{this.props.item}</li>
+    );
+  }
+};
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"))
